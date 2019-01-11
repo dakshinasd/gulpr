b2b321349842aab12e2d7f8ae8bf00331adec1b4
Dakshina Wijesuriya <me@dakshinasd.com>
2019-01-11 09:29:50 +0530

    Added JS minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 
 const sass = require('gulp-sass');
-livereload = require('gulp-livereload');
+const minify = require('gulp-minify');
 
 const paths = {
     sass: {
@@ -10,13 +10,13 @@ const paths = {
     },
 
     js: {
-        src: 'src/js/*.js',
-        dest: 'dist'
+        src: './src/js/*.js',
+        dest: './dist/js'
     },
 
     images: {
       src: 'src/images/**/*.{jpg,jpeg,png}',
-      dest: 'build/img/'
+      dest: 'dist/img/'
     },
 
     html: {
@@ -38,18 +38,26 @@ function convertSASS(){
     return gulp.src('./src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css/'))
-        
+
+}
+
+function processJS(){
+    return gulp.src(paths.js.src)
+    .pipe(minify())
+    .pipe(gulp.dest(paths.js.dest))
 }
 
 function watch() {
     gulp.watch(paths.html.src, copyHTML);
     gulp.watch(paths.sass.src, convertSASS);
+    gulp.watch(paths.js.src, processJS)
   }
 
 var build = gulp.series(copyHTML, convertSASS, watch)
 
 exports.copyHTML = copyHTML;
 exports.convertSASS = convertSASS;
+exports.processJS = processJS;
 exports.watch = watch;
 exports.build = build;
 
